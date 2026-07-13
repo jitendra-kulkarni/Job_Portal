9c1ad43d4ae912a099acc0cbdbd4a98b44ec687e
Jitendra Kulkarni <jitendrakulkarni01@gmail.com>
2026-07-13 12:43:57 +0530

    Complete Job module with role based and object-level permissions

--- backend/accounts/permissions.py
+++ backend/accounts/permissions.py
@@ -3,4 +3,11 @@ from rest_framework.permissions import BasePermission
 class IsEmployer(BasePermission):
 
     def has_permission(self, request, view):
-        return request.user.role == "EMPLOYER"
+        return (
+            request.user.is_authenticated and
+            request.user.role == "EMPLOYER"
+        )
+    
+class IsJobOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.company == request.user.company
--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from .models import Job
 from .serializers import JobSerializer
+from accounts.permissions import IsEmployer
+from accounts.permissions import IsJobOwner
 
-# Create your views here.
+
 class JobViewSets(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [IsAuthenticatedOrReadOnly()]
+
+        return [IsEmployer()]
+
+    def perform_create(self, serializer):
+        serializer.save(company=self.request.user.company)
+
+
+    def get_permissions(self):
+
+        if self.action in ["list", "retrieve"]:
+            return [IsAuthenticatedOrReadOnly()]
+        
+        elif self.action == "create":
+            return [IsEmployer()]
+        
+        return [IsEmployer(), IsJobOwner()]
