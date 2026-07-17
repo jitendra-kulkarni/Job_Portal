7bb95f677479dfe4cd4d7772214525d6674ad919
Jitendra Kulkarni <jitendrakulkarni01@gmail.com>
2026-07-17 19:52:31 +0530

    small change

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -45,16 +45,27 @@ INSTALLED_APPS = [
     'companies',
     'jobs',
     'applications',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
+
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
     'django.contrib.messages.middleware.MessageMiddleware',
+
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -136,3 +147,12 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- backend/jobs/serializers.py
+++ backend/jobs/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from .models import Job
 
 class JobSerializer(serializers.ModelSerializer):
+    company_name = serializers.CharField(
+        source = "company.company_name",
+        read_only = True
+    )
+    
     class Meta:
         model = Job
         fields = "__all__"
